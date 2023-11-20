2bf674f5bb37115fde56b155004dbaadd1f79290
agusdwimilniadi <agusdwimill@gmail.com>
2023-11-20 12:52:16 +0700

    init react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { AuthProvider } from 'react-auth-kit';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <div className="text-3xl underline">Home</div>,
+    },
+  ]);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AuthProvider
+        authName="_auth"
+        authType="cookie"
+        cookieDomain={window.location.hostname}
+        cookieSecure={window.location.protocol === 'https:'}
+      >
+        <RouterProvider router={router} />
+      </AuthProvider>
     </>
-  )
+  );
 }
-
-export default App
---
+++ client/src/pages/Home/index.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className="text-center underline">Home</div>;
+};
+
+export default Home;
